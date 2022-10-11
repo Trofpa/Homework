77e3e8c5ec1c49034d826b00e402871ded6d425a
Trofpa <yaseinokaminari@gmail.com>
2022-10-11 23:00:02 +0300

    Task 29 added

--- seminar4/Task29/Program.cs
+++ seminar4/Task29/Program.cs
@@ -8,3 +8,19 @@
 
 Автор: Трофимов П.А.
 */
+
+//Метод:
+int[] GenArray(int Nelements, int Arrange)
+{
+    int[] array = new int[Nelements];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, Arrange + 1);
+    }
+    Console.WriteLine($"[{String.Join(", ", array)}]");
+    return array;
+}
+
+
+//Программа для вызова массива из  элементов:
+int [] ex = GenArray(Nelements: 8, Arrange: 50);
--- seminar4/Task60_Extra/Program.cs
+++ seminar4/Task60_Extra/Program.cs
@@ -16,3 +16,69 @@
 
 Автор: Трофимов П.А.
 */
+
+//Одномерный случай:
+/*
+int[] GenArray()
+{
+    Console.WriteLine("Создаем массив целых чисел."
+    + "\nСколько хотите элементов в массиве?"
+    + "\nВведите целое число: ");
+    int length = int.Parse(Console.ReadLine());
+    int [] array = new int[length];
+
+    Console.WriteLine("\nДо какого диапазона значения?");
+    int Numbs = int.Parse(Console.ReadLine());
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, Numbs + 1);
+    }
+    Console.WriteLine($"Сгенерированный массив: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write("  "+ $"{array[i]}");
+    }
+    return array;
+}
+
+int[] HowManyTimes(int[] array)
+{
+    int max = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > max) max = array[i];
+    }
+    
+    int[] AddArray = new int[max + 1];
+    for (int i = 0; i < AddArray.Length; i++)
+    {
+        for (int j = 0; j < array.Length; j++)
+        {
+            if(array[j] == i)
+            {
+                AddArray[i]++;
+            }
+        }
+    }
+    return AddArray;
+}
+
+void PrintResult(int[] array)
+{
+    Console.WriteLine();
+    for (int i = 0; i < array.Length; i++)
+{
+    if(array[i] != 0) Console.WriteLine($"Количество {i} - {array[i]} раза;");
+}
+}
+
+
+int[] array1 = GenArray();
+
+int[] array2 = HowManyTimes(array1);
+
+PrintResult(array2);
+
+*/
+
