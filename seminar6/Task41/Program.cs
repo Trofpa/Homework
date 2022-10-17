26427f31c26c89270c66c5efc457805c33643614
Trofpa <yaseinokaminari@gmail.com>
2022-10-17 20:58:18 +0300

    seminar6 homework added

---
+++ seminar6/Task41/Program.cs
@@ -0,0 +1,63 @@
+﻿/* Условие задачи
+41.  Пользователь вводит с клавиатуры M чисел.
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+`0, 7, 8, -2, -2 -> 2`
+`1, -7, 567, 89, 223-> 3`
+
+Автор: Трофимов П.А.
+
+*/
+
+//Методы
+
+int InputNumber(string text)
+{
+    bool Condition = true;
+    int number = 0;
+    while (Condition)
+    {
+        Console.Write(text);
+        if (int.TryParse(Console.ReadLine(), out number))
+        {
+            Condition = false;
+        }
+        else
+        {
+            Console.WriteLine("Введенный символ не является числом!");
+        }
+    }
+    return number;
+}
+
+int[] FillString(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = InputNumber($"Введите {i + 1}-е число: ");
+    }
+    return result;
+}
+
+int CountPos(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            result++;
+        }
+    }
+    return result;
+}
+
+
+//Программа:
+int N = InputNumber("Сколько чисел вы хотите ввести: ");
+
+int[] array = FillString(N);
+
+Console.WriteLine("Счет положительных чисел..."
+                + $"\n{String.Join(", ", array)} -> {CountPos(array)}");
---
+++ seminar6/Task43/Program.cs
@@ -0,0 +1,90 @@
+﻿/*Условие задачи
+№43.  Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями
+y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+`b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)`
+
+Автор: Трофимов П.А.
+
+*/
+
+//Методы
+double InputNumber(string text)
+{
+    bool Condition = true;
+    double number = 0;
+    while (Condition)
+    {
+        Console.Write(text);
+        if (double.TryParse(Console.ReadLine(), out number))
+        {
+            Condition = false;
+        }
+        else
+        {
+            Console.WriteLine("Введенный символ не является числом!");
+        }
+    }
+    return number;
+}
+
+double[] InputLine(bool show = true)
+{
+    Console.WriteLine("Ввод прямой y = kx + b.\n");
+    double[] result =
+    {
+    InputNumber("Введите коэффициент k: "),
+    InputNumber("Введите коэффициент b: ")
+    };
+    if (show) Console.WriteLine($"\nВведена прямая {result[0]}x + {result[1]}"
+                                +"\n-----------------------------------------");
+    return result;
+}
+
+bool[] Intersection(double[] line1, double[] line2)
+{
+    bool[] result = new bool[2];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = line1[i] == line2[i];
+    }
+    return result;
+}
+
+double[] IntersPoint(double[] line1, double[] line2)
+{
+    double[] result = new double[2];
+    result[0] = (line2[1] - line1[1]) / (line1[0] - line2[0]); // точка Х
+    result[1] = (line1[0] * line2[1] - line1[1] * line2[0]) / (line1[0] - line2[0]); // точка Y
+    return result;
+}
+
+
+//Программа
+
+double[] line1 = InputLine();
+double[] line2 = InputLine();
+
+bool[] condition = Intersection(line1, line2);
+
+if (condition[0])
+{
+    if (condition[1])
+    {
+        Console.WriteLine($"Прямые {line1[0]}x + {line1[1]} и "
+                            + $"{line2[0]}x + {line2[1]} идентичны");
+    }
+    else
+    {
+        Console.WriteLine($"Прямые {line1[0]}x + {line1[1]} и "
+                        + $"{line2[0]}x + {line2[1]} параллельны");
+    }
+}
+else
+{
+    Console.WriteLine($"Пересечение {line1[0]}x + {line1[1]}"
+                    + $" и {line2[0]}x + {line2[1]} -> "
+                    + $"({String.Join("; ", IntersPoint(line1, line2)):N2)})");
+}
+
+
