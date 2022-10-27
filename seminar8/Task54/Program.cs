506662ab3968aa63e598579a14bd20a174c01e01
Trofpa <yaseinokaminari@gmail.com>
2022-10-27 21:32:56 +0300

    seminar8 homework added

--- seminar8/Task54/Program.cs
+++ seminar8/Task54/Program.cs
@@ -1,2 +1,82 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Условия задачи:
+Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+
+`1 4 7 2`
+`5 9 2 3`
+`8 4 2 4`
+
+В итоге получается вот такой массив:
+
+`7 4 2 1`
+`9 5 3 2`
+`8 4 4 2`
+
+Автор: Трофимов П.А.
+*/
+
+//Метод для заполнения двумерного массива из целых чисел
+int[,] FillArray(int[,] array, int min = 0, int max = 10)
+{
+    //Заполняем двумерный массив случайными значениями
+    int rows = array.GetLength(0);
+    int cols = array.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            array[i, j] = Random.Shared.Next(min, max + 1);
+        }
+    }
+    return array;
+}
+//Метод сортировки строк
+int[,] SortArray(int[,] array)
+{
+    bool condition = true;
+    int temp;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        do
+        {
+            condition = false;
+            for (int j = 0; j < array.GetLength(1) - 1; j++)
+            {
+                if (array[i, j] < array[i, j + 1])
+                {
+                    condition = true;
+                    temp = array[i, j + 1];
+                    array[i, j + 1] = array[i, j];
+                    array[i, j] = temp;
+                }
+            }
+        } while (condition);
+    }
+    return array;
+}
+//Вывод матрицы в консоль
+void PrintArray(int[,] array, string text = "\nСгенерированный массив: \n")
+{
+    Console.WriteLine(text);
+    int rows = array.GetLength(0);
+    int cols = array.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+    Console.Write("\t");
+        for (int j = 0; j < cols; j++)
+        {
+            Console.Write($" {array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+//Программа
+Console.Clear();
+int[,] matrixOld = new int[6, 5];
+matrixOld = FillArray(matrixOld, max:9);
+PrintArray(matrixOld, "Начальная матрица: ");
+SortArray(matrixOld);
+PrintArray(matrixOld, "Отсортированная матрица: ");
+
--- seminar8/Task56/Program.cs
+++ seminar8/Task56/Program.cs
@@ -1,2 +1,82 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Условие задачи:
+56. Задайте прямоугольный двумерный массив. Напишите программу,
+которая будет находить строку с наименьшей суммой элементов.
+
+Например, задан массив:
+
+`1 4 7 2`  
+`5 9 2 3`  
+`8 4 2 4`  
+`5 2 6 7`  
+
+`Ответ: 1 строка`
+
+Автор: Трофимов П.А.
+
+*/
+
+//Метод для заполнения двумерного массива из целых чисел
+int[,] FillArray(int[,] array, int min = 0, int max = 10)
+{
+    //Заполняем двумерный массив случайными значениями
+    int rows = array.GetLength(0);
+    int cols = array.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            array[i, j] = Random.Shared.Next(min, max + 1);
+        }
+    }
+    return array;
+}
+
+//Вывод матрицы в консоль
+void PrintArray(int[,] array, string text = "\nСгенерированный массив: \n")
+{
+    Console.WriteLine(text);
+    int rows = array.GetLength(0);
+    int cols = array.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+    Console.Write("\t");
+        for (int j = 0; j < cols; j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+//Поиск строки с минимальной суммой
+int MinRow(int[,] array)
+{
+    int result = 1;
+    int minSum = 0;
+    int cash = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            cash += array[i, j];
+        }
+        if(i == 0) minSum = cash;
+        else
+        {
+            if(cash < minSum)
+            {
+                result = i + 1;
+                minSum = cash;
+            }
+        }
+        cash = 0;
+    }
+    return result;
+}
+
+//Программа
+int[,] matrix = new int[3,2];
+matrix = FillArray(matrix, 0, 50);
+PrintArray(matrix);
+Console.WriteLine($"Ответ: {MinRow(matrix)} строка.");
