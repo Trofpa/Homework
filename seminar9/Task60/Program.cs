dd318272f35765642f64019754cdcd13c51121de
Trofpa <yaseinokaminari@gmail.com>
2022-11-13 17:46:45 +0300

    added tasks for homework9

---
+++ seminar9/Task60/Program.cs
@@ -0,0 +1,64 @@
+﻿/*
+## Задача 60:  
+Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.
+Напишите программу, которая будет построчно выводить массив,
+добавляя индексы каждого элемента.  
+
+Пример: Массив размером 2 x 2 x 2
+
+66(0,0,0) 25(0,1,0) 
+34(1,0,0) 41(1,1,0)  
+27(0,0,1) 90(0,1,1) 
+26(1,0,1) 55(1,1,1) 
+
+
+Автор: Трофимов П.А.
+
+*/
+//Метод для заполнения массива
+int[,,] FillArray(int[,,] array, int min, int max)
+{
+    List<int> numbers = new List<int>();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = Random.Shared.Next(min, max + 1);
+                while (numbers.Contains(array[i, j, k]))
+                {
+                    array[i, j, k] = Random.Shared.Next(min, max + 1);
+                }
+                numbers.Add(array[i, j, k]);
+            }
+        }
+    }
+    return array;
+}
+
+
+//Метод для отображения массива с добавлением индексов
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]}({i},{j},{k}) ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+
+//Программа
+int[,,] matrix = new int[2, 2, 2];
+
+matrix = FillArray(matrix, 10, 99);
+PrintArray(matrix);
+Console.WriteLine();
---
+++ seminar9/Task68/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Задача 68:  
+Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.  
+
+Пример:  
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29 
+
+Автор: Трофимов П.А.
+
+*/
+
+//Метод на функцию Аккермана
+int Akkerman(int M, int N)
+{
+    if(M == 0) return N + 1;
+    else
+    {
+        if(N == 0) return Akkerman(M - 1, 1);
+        else return Akkerman(M - 1, Akkerman(M, N - 1));
+    }
+}
+
+//Программа
+int m = 3;
+int n = 2;
+
+Console.WriteLine($"m = {m}, n = {n} -> A(m,n) = {Akkerman(m, n)}");
